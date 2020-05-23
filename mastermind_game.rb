fda9c70f5ecf945a2ba95233cc85fa6b30d8cbde
Sef Hill <sefhill@icloud.com>
2020-05-23 21:55:30 +0100

    Gameplay pseudo code written

--- mastermind_game.rb
+++ mastermind_game.rb
@@ -3,13 +3,36 @@ require_relative 'console'
 class Mastermind
     include Console
 
-    def initialize
+    #CONSTANTS: the 6 colours
 
+    def initialize
+        #?
+    end
+
+    def set_puzzle
+        #computer randomly chooses 4 colours out of the 6 CONSTANTS
+        #- are repeat colours allowed?
+        #the 4 colours are assigned to the 4 positions
+        #- local variable, type array
     end
 
     def play
+        set_puzzle
 
-
+        #loop
+            #get_user_guess
+            #if correct
+                #user wins
+                #run win script
+                #return
+            #elsif 12th go
+                #user loses
+                #run lose script
+                #return
+            #else
+                #give_feedback
+            #end
+        #end 
 
     end
 end
