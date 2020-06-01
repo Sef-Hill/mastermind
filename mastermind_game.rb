432bd7febeef876c539fd430706df4cfa545c885
Sef Hill <sefhill@icloud.com>
2020-06-01 10:43:25 +0100

    More removal of commented out code

--- mastermind_game.rb
+++ mastermind_game.rb
@@ -34,46 +34,4 @@ class Mastermind
             if @keep_playing then @player_role = get_player_role end
         end
     end
-
-    # def give_feedback(user_guess)
-    #     feedback = get_feedback(user_guess)
-    #     display_feedback(feedback)
-    # end
-
-    # def get_feedback(user_guess)
-    #     #exact matches
-    #     solution_copy = @solution.clone
-    #     user_guess_copy = user_guess.clone
-    #     feedback = {}
-    #     indices = []
-    #     exact_matches = 0
-    #     #count exact matches
-    #     solution_copy.each_with_index do |number, index|
-    #         if user_guess[index] == number
-    #             exact_matches += 1
-    #             indices.push(index)
-    #         end
-    #     end
-    #     feedback[:exact] = exact_matches
-
-    #     #close matches
-    #     #first, remove exact matches
-    #     indices.reverse!
-    #     indices.each do |i|
-    #         solution_copy.delete_at(i)
-    #         user_guess_copy.delete_at(i)
-    #     end
-    #     #count close matches
-    #     close_matches = 0
-    #     user_guess_copy.each do |number|
-    #         if solution_copy.include?(number)
-    #             #delete 'number' from solution_copy
-    #             solution_copy.delete_at(solution_copy.index(number))
-    #             close_matches += 1
-    #         end
-    #     end
-    #     feedback[:close] = close_matches
-
-    #     return feedback
-    # end
 end
